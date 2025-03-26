ade1e9a664906ef01ffa93839f24078659beb0cd
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-03-27 01:42:11 +0300

    Arrays task

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,12 +1,35 @@
 module Exercise
   module Arrays
     class << self
-      def replace(array)
-        array
+      def my_max(array)
+        max = 0
+
+        array.each do |el|
+          max = el if el > max
+        end
+        max
       end
 
-      def search(_array, _query)
-        0
+      def replace(array)
+        max = my_max(array)
+
+        array.each_with_index do |el, i|
+          array[i] = max if el >= 0
+        end
+      end
+
+      def search(array, query, low = 0, high = array.size - 1)
+        return - 1 if low > high
+
+        mid = (low + high) / 2
+
+        if array[mid] < query
+          search(array, query, mid + 1, high)
+        elsif array[mid] > query
+          search(array, query, low, mid - 1)
+        else
+          mid
+        end
       end
     end
   end
