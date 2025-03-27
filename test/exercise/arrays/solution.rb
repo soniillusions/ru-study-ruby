2bffe9f8f875edbedb1f8f9e2106122addd80fe0
Alexey Maryshenkov <alekseycrosso@gmail.com>
2025-03-27 13:46:32 +0300

    fix arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -3,33 +3,24 @@ module Exercise
     class << self
       def my_max(array)
         max = 0
-
-        array.each do |el|
-          max = el if el > max
-        end
+        array.map { |el| max = el if el > max }
         max
       end
 
       def replace(array)
         max = my_max(array)
-
-        array.each_with_index do |el, i|
-          array[i] = max if el >= 0
-        end
+        array.map { |el| el >= 0 ? max : el }
       end
 
       def search(array, query, low = 0, high = array.size - 1)
-        return - 1 if low > high
+        return -1 unless array.include?(query)
+        return -1 if low > high
 
         mid = (low + high) / 2
 
-        if array[mid] < query
-          search(array, query, mid + 1, high)
-        elsif array[mid] > query
-          search(array, query, low, mid - 1)
-        else
-          mid
-        end
+        return mid if array[mid] == query
+
+        array[mid] < query ? search(array, query, mid + 1, high) : search(array, query, low, mid - 1)
       end
     end
   end
